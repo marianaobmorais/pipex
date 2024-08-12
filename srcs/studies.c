40fa056b353db41194d8e7f6a3379ef10292b32d
Mariana Oliveira Braga De Morais <mariaoli@c2r1s8.42porto.com>
2024-08-12 17:56:18 +0100

    No project yet, just exercises to practice pipe and fork functions

--- srcs/studies.c
+++ srcs/studies.c
@@ -1,24 +1,95 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   studies.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mariaoli <mariaoli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 17:44:47 by mariaoli          #+#    #+#             */
-/*   Updated: 2024/08/12 17:11:57 by mariaoli         ###   ########.fr       */
+/*   Updated: 2024/08/12 17:53:15 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 #include <stdio.h>
+#include <sys/wait.h>
 
-//The child proccess goes first and should end first
+/* int fork1(void)
+{
+	int pid;
+
+	pid = fork();
+	if (pid == -1)
+		exit(1);
+	return (pid);
+} */
 
 int	main(int ac, char **av)
 {
-/* 	if (!valid_args(ac, av))
-		return (EXIT_FAILURE); */
+	int	arr[] = {1, 2, 3, 4, 1, 2, 7};
+	int	arr_size = sizeof(arr) / sizeof(int);
+	int	start;
+	int	end;
+	int	fd[2];
+	int	id;
+	int	i;
+	int	sum;
+	int	sum_child;
+	int	total_sum;
+
+	if (pipe(fd) == -1)
+		return (1);
+	id = fork();
+/* 	if (fork1() == 0)
+		// child
+	// parent */
+	if (id == -1)
+		return (2);
+	if (id == 0) // child process
+	{
+		start = 0;
+		end = arr_size / 2;
+	}
+	else // parent process
+	{
+		start = arr_size / 2;
+		end = arr_size;
+	}
+	sum = 0;
+	i = start;
+	while (i < end)
+	{
+		sum += arr[i];
+		i++;
+	}
+	ft_printf("Calculated partial sum: %d\n", sum);
+	if (id == 0) //send partial sum from child to parent
+	{
+		close (fd[0]);
+		if (write(fd[1], &sum, sizeof(sum)) == -1) // sizeof(int) also works
+			return (1);
+		close(fd[1]);
+	}
+	else
+	{
+		close(fd[1]);
+		if (read(fd[0], &sum_child, sizeof(sum_child)) == -1)
+			return (1);
+		close(fd[0]);
+		total_sum = sum + sum_child;
+		ft_printf("Total sum is: %d\n", total_sum);
+		wait(NULL); // wait for the child process to finish before starting parent process
+	}
+	return (0);
+}
+
+
+//The child process goes first and should end first
+
+/* int	main(int ac, char **av)
+{
+	// if (!valid_args(ac, av))
+	// 	return (EXIT_FAILURE);
 
 	int	fd[2];
 	// two different fd for two different ends of a pipe
@@ -56,4 +127,4 @@ int	main(int ac, char **av)
 	}
 	
 	return (0);
-}
+} */
